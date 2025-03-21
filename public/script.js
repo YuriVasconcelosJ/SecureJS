1013eac28af1470be4f8f360af19e2927cfd5409
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2025-03-21 02:26:27 -0300

    feat: adiciona funcionalidade de login
    - Adicionando funcionalidade no botão de login para aparecer a model de login.
    - Início da criação da tela de login

---
+++ public/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Declaração de variáveis
+const buttonLogin = document.querySelector(".login");
+const buttonRegister = document.querySelector(".signup");
+const modalElement = document.querySelector(".modal");
+
+// Função para abrir ou fechar a tela
+function toggleModal(displayStyle) {
+  modalElement.style.display = displayStyle;
+}
+
+buttonLogin.addEventListener("click", () => {
+  toggleModal("block");
+});
